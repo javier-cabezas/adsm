01e3d91cc79e372e17a6e5820c4c42971a1d018d
igelado <igelado@localhost>
2009-03-06 10:59:32 +0100

    [svn r13] Added missing file cuda.cpp

---
+++ src/lib/cuda.cpp
@@ -0,0 +1,19 @@
+#include "gmac.h"
+
+#include <dlfcn.h>
+#include <cuda_runtime.h>
+
+#include <common/debug.h>
+
+cudaError_t (*_cudaLaunch)(const char *) = NULL;
+
+static void __attribute__((constructor)) gmacCudaInit(void)
+{
+	if((_cudaLaunch = (cudaError_t (*)(const char *))dlsym(RTLD_NEXT, "cudaLaunch")) == NULL)
+		FATAL("cudaLaunch not found");
+}
+
+cudaError_t cudaLaunch(const char *symbol)
+{
+	return gmacLaunch(symbol);
+}
