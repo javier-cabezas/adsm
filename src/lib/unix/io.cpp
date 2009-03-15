d6f88cf6ffb3662a9fa5fa2c4c9446c719b7069a
igelado <igelado@localhost>
2009-03-15 01:37:48 +0100

    [svn r27] Added initial IO support

---
+++ src/lib/unix/io.cpp
@@ -0,0 +1,34 @@
+#include <common/debug.h>
+
+#ifdef PARAVER
+#include <gmac/paraver.h>
+#endif
+
+#include <dlfcn.h>
+#include <unistd.h>
+
+typedef ssize_t (*read_t)(int, void *, size_t);
+typedef ssize_t (*write_t)(int, const void *, size_t);
+
+static read_t _read = NULL;
+static write_t _write = NULL;
+
+static void __attribute__((constructor)) gmacPthreadInit(void)
+{
+	if((_read = (read_t)dlsym(RTLD_NEXT, "read")) == NULL)
+		FATAL("Could not find read()");
+	if((_write = (write_t)dlsym(RTLD_NEXT, "write")) == NULL)
+		FATAL("Could not find write()");
+}
+
+ssize_t read(int fd, void *buf, size_t count)
+{
+	TRACE("read");
+	return _read(fd, buf, count);
+}
+
+ssize_t write(int fd, const void *buf, size_t count)
+{
+	TRACE("write");
+	return _write(fd, buf, count);
+}
