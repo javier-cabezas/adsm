184750f0dd08f6c9ad234d642d23861356404341
igelado <igelado@localhost>
2009-03-15 22:06:45 +0100

    [svn r32] Changed EINTR to EFAULT in I/O handling routines

--- src/lib/unix/io.cpp
+++ src/lib/unix/io.cpp
@@ -36,7 +36,7 @@ ssize_t read(int fd, void *buf, size_t count)
 	uint8_t *ptr = (uint8_t *)buf;
 	do {
 		n += __libc_read(fd, ptr + n, count - n);
-	} while(n < count && errno == EINTR);
+	} while(n < count && errno == EFAULT);
 	popState();
 	return n;
 }
@@ -49,7 +49,7 @@ ssize_t write(int fd, const void *buf, size_t count)
 	uint8_t *ptr = (uint8_t *)buf;
 	do {
 		n += __libc_write(fd, ptr + n, count - n);
-	} while(n < count && errno == EINTR);
+	} while(n < count && errno == EFAULT);
 	popState();
 	return n;
 }
@@ -63,12 +63,13 @@ size_t fread(void *buf, size_t size, size_t nmemb, FILE *stream)
 	uint8_t *ptr = (uint8_t *)buf;
 	do {
 		n += __libc_fread(ptr + (n * size), size, nmemb - n, stream);
-		if(ferror(stream) && errno == EINTR) clearerr(stream);
-	} while(ferror(stream) == 0);
+		if(ferror(stream) && errno == EFAULT) clearerr(stream);
+	} while(n < nmemb && ferror(stream) == 0);
 	popState();
 	return n;
 }
 
+#include <string.h>
 
 size_t fwrite(const void *buf, size_t size, size_t nmemb, FILE *stream)
 {
@@ -77,8 +78,9 @@ size_t fwrite(const void *buf, size_t size, size_t nmemb, FILE *stream)
 	uint8_t *ptr = (uint8_t *)buf;
 	do {
 		n += __libc_fwrite(ptr + (n * size), size, nmemb - n, stream);
-		if(ferror(stream) && errno == EINTR) clearerr(stream);
-	} while(ferror(stream) == 0);
+		fprintf(stderr,"ACHTUNG: %d written (%s)\n", n, strerror(errno));
+		if(ferror(stream) && errno == EFAULT) clearerr(stream);
+	} while(n < nmemb && ferror(stream) == 0);
 	popState();
 	return n;
 }
