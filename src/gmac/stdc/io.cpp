e609795fda4c7d935ade529e3479993a32943d34
jcabezas <jcabezas@localhost>
2009-09-22 19:50:28 +0200

    [svn r390] Fixed fread fwrite

--- src/gmac/stdc/io.cpp
+++ src/gmac/stdc/io.cpp
@@ -4,6 +4,10 @@
 #include <debug.h>
 #include <paraver.h>
 
+#include <init.h>
+#include <memory/MemManager.h>
+#include <kernel/Context.h>
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -22,47 +26,55 @@ static void __attribute__((constructor(INTERPOSE))) stdcInit(void)
 }
 
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /* Standard C library wrappers */
 
+#ifdef __cplusplus
+extern "C"
+#endif
 size_t fread(void *buf, size_t size, size_t nmemb, FILE *stream)
 {
+	ssize_t n = 0;
+	uint8_t *ptr = (uint8_t *)buf;
+    gmac::Context *ctx = manager->owner(buf);
+
+    if(ctx == NULL) return __libc_fread(buf, size, nmemb, stream);
+
 	pushState(IORead);
-	ssize_t n = 0;
-	uint8_t *ptr = (uint8_t *)buf;
-	do {
-		n += __libc_fread(ptr + (n * size), size, nmemb - n, stream);
-		if(feof(stream)) break;
-		if(ferror(stream) && errno == EFAULT) {
-			clearerr(stream);
-			if(n > 0) FATAL("Interrupted System Call");
-		}
-	} while(n < nmemb && ferror(stream) == 0);
-	popState();
-	return n;
-}
 
+    manager->invalidate(buf, size * nmemb);
+    void *tmp = malloc(size * nmemb);
 
-size_t fwrite(const void *buf, size_t size, size_t nmemb, FILE *stream)
-{
-	pushState(IOWrite);
-	ssize_t n = 0;
-	uint8_t *ptr = (uint8_t *)buf;
-	do {
-		n += __libc_fwrite(ptr + (n * size), size, nmemb - n, stream);
-		if(ferror(stream) && errno == EFAULT) {
-			clearerr(stream);
-			if(n > 0) FATAL("Interrupted System Call");
-		}
-	} while(n < nmemb && ferror(stream) == 0);
-	popState();
-	return n;
+    size_t ret = __libc_fread(tmp, size, nmemb, stream);
+    ctx->copyToDevice(manager->safe(buf), tmp, size * nmemb);
+    free(tmp);
+    popState();
+
+	return ret;
 }
 
 
 #ifdef __cplusplus
-}
+extern "C"
 #endif
+size_t fwrite(const void *buf, size_t size, size_t nmemb, FILE *stream)
+{
+	ssize_t n = 0;
+	uint8_t *ptr = (uint8_t *)buf;
+    gmac::Context *ctx = manager->owner(buf);
+
+    if(ctx == NULL) return __libc_fwrite(buf, size, nmemb, stream);
+
+	pushState(IOWrite);
+
+    void *tmp = malloc(size * nmemb);
+
+    manager->flush(buf, size * nmemb);
+    ctx->copyToHost(tmp, manager->safe(buf), size * nmemb);
+
+    size_t ret =  __libc_fwrite(tmp, size, nmemb, stream);
+    free(tmp);
+	
+    popState();
+
+    return ret;
+}
