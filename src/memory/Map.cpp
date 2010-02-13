88eaedb09bf763bc38057e0de11104044b5e99e2
Isaac Gelado <igelado@ac.upc.edu>
2010-02-13 01:07:15 +0100

    Fixed dead-iterator usage in memory::Map

--- src/memory/Map.cpp
+++ src/memory/Map.cpp
@@ -79,13 +79,14 @@ Region *Map::remove(void *addr)
 	global.write();
 	i = __global->upper_bound(addr);
 	assert(i != __global->end() && i->second->start() == addr);
-	if(i->second->owner() == gmac::Context::current()) __global->erase(i);
+    Region *region = i->second;
+    bool self = (region->owner() == gmac::Context::current());
+    if(self == true) __global->erase(i);
 	global.unlock();
 	// If the region is global (not owned by the context) return
-	if(i->second->owner() != gmac::Context::current()) 
-		return i->second;
+    if(self == false) return region;
 
-	TRACE("Removing Region %p", i->second->start());
+	TRACE("Removing Region %p", region->start());
 	local.write();
 	i = __map.upper_bound(addr);
 	assert(i != __map.end() && i->second->start() == addr);
