d854356b9f4544b1af8340825ed39a8e53ed21bd
Isaac Gelado <igelado@ac.upc.edu>
2010-05-13 18:44:27 +0200

    Fixed compilation in debug mode

--- src/memory/Allocator.cpp
+++ src/memory/Allocator.cpp
@@ -6,7 +6,7 @@ namespace gmac { namespace memory {
 
 Allocator *getAllocator(Manager *manager, const char *allocatorName)
 {
-    if(allocatorName == NULL) return new allocator::Slab(manager)
+    if(allocatorName == NULL) return new allocator::Slab(manager);
     TRACE("Using %s Allocator", allocatorName);
     if(strcasecmp(allocatorName, "None") == 0)
         return NULL;
