b421be13321ac4d344d3669efba5f5745245cddc
Isaac Gelado <igelado@ac.upc.edu>
2010-09-17 03:29:44 +0200

    Added tracing to Context

--- src/kernel/Context.cpp
+++ src/kernel/Context.cpp
@@ -2,6 +2,7 @@
 
 #include <memory/Manager.h>
 #include <trace/Thread.h>
+#include <trace/Function.h>
 
 namespace gmac {
 
@@ -21,17 +22,26 @@ Context::~Context()
 
 gmacError_t Context::copyToDevice(void *dev, const void *host, size_t size)
 {
-    return _acc->copyToDevice(dev, host, size);
+    trace::Function::start("Context", "copyToDevice");
+    gmacError_t ret = _acc->copyToDevice(dev, host, size);
+    trace::Function::end("Context");
+    return ret;
 }
 
 gmacError_t Context::copyToHost(void *host, const void *dev, size_t size)
 {
-    return _acc->copyToHost(host, dev, size);
+    trace::Function::start("Context", "copyToHost");
+    gmacError_t ret = _acc->copyToHost(host, dev, size);
+    trace::Function::end("Context");
+    return ret;
 }
 
 gmacError_t Context::copyDevice(void *dst, const void *src, size_t size)
 {
-    return _acc->copyDevice(dst, src, size);
+    trace::Function::start("Context", "copyDevice");
+    gmacError_t ret = _acc->copyDevice(dst, src, size);
+    trace::Function::end("Context");
+    return ret;
 }
 
 }
