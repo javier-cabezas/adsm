09138dcb1d399f60900b3b25ae82fb83bd77fbc3
jcabezas <jcabezas@localhost>
2010-09-18 05:34:07 +0200

    Fixed gmacMemset test

--- tests/gmacMemset.cu
+++ tests/gmacMemset.cu
@@ -30,30 +30,16 @@ int main(int argc, char *argv[])
 	if(size % blockSize) Db.x++;
 
 	fprintf(stderr,"Test full memset: ");
-	//gmacMemset(ptr, 0, size * sizeof(long));
-	memset(ptr, 1, size * sizeof(long));
-	memset(ptr, 0, size * sizeof(long));
-
-    for (int i = 0; i < size; i++) {
-        if (ptr[i] != 0) {
-            printf("WTF %d\n", i);
-        }
-    }
+    //gmacMemset(ptr, 0, size * sizeof(long));
+    memset(ptr, 0, size * sizeof(long));
 
 	reset<<<Dg, Db>>>(gmacPtr(ptr), 1);
     gmacThreadSynchronize();
 	fprintf(stderr,"%d\n", check(ptr, size));
 
-    for (int i = 0; i < size; i++) {
-        if (ptr[i] != 1) {
-            printf("WTF2 %d\n", i);
-        }
-    }
-
 	fprintf(stderr, "Test partial memset: ");
 	//gmacMemset(&ptr[size / 8], 0, 3 * size / 4 * sizeof(long));
-	memset(ptr, 0, size * sizeof(long));
-    gmacThreadSynchronize();
+	memset(&ptr[size / 8], 0, 3 * size / 4 * sizeof(long));
 	fprintf(stderr,"%d\n", check(ptr, size / 4));
 
 	gmacFree(ptr);
