b85deb244de780ff282efd650477ee2bc074fb77
jcabezas <jcabezas@localhost>
2010-10-25 18:13:51 +0200

    Added signed keyword to type definition

--- src/config/common.h
+++ src/config/common.h
@@ -41,11 +41,11 @@ WITH THE SOFTWARE.  */
 #include <stdint.h>
 #else
 typedef unsigned char uint8_t;
-typedef char int8_t;
+typedef signed char int8_t;
 typedef unsigned short uint16_t;
-typedef short int16_t;
+typedef signed short int16_t;
 typedef unsigned int uint32_t;
-typedef int int32_t;
+typedef signed int int32_t;
 #endif
 
 #ifndef _MSC_VER
