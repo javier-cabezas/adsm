4e9155cd611a1241adc4a5395707dfc957ed9648
jcabezas <jcabezas@localhost>
2010-11-04 16:30:29 +0100

    Fixed wrong call to implementation

---
+++ src/api/cuda/driver.cpp
@@ -0,0 +1,130 @@
+#include <cstring>
+
+#include <vector_types.h>
+#include <driver_types.h>
+
+#include <string>
+#include <vector>
+
+#include "include/gmac.h"
+#include "gmac/init.h"
+
+#include "core/Kernel.h"
+
+#include "Accelerator.h"
+#include "Mode.h"
+#include "Module.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+using gmac::cuda::Accelerator;
+using gmac::cuda::Mode;
+using gmac::KernelDescriptor;
+using gmac::cuda::ModuleDescriptor;
+using gmac::cuda::TextureDescriptor;
+using gmac::cuda::VariableDescriptor;
+
+/*!
+ * @returns Module **
+ */
+GMAC_API void **__cudaRegisterFatBinary(void *fatCubin)
+{
+    gmac::Process &proc = gmac::Process::getInstance();
+    gmac::util::Logger::TRACE("CUDA Fat binary: %p", fatCubin);
+    gmac::util::Logger::ASSERTION(proc.nAccelerators() > 0);
+    gmac::enterGmac();
+    // Use the first GPU to load the fat binary
+    void **ret = (void **) new ModuleDescriptor(fatCubin);
+	gmac::exitGmac();
+	return ret;
+}
+
+GMAC_API void __cudaUnregisterFatBinary(void **fatCubinHandle)
+{
+    ModuleDescriptor *mod = (ModuleDescriptor *)fatCubinHandle;
+    delete mod;
+}
+
+GMAC_API void __cudaRegisterFunction(
+		void **fatCubinHandle, const char *hostFun, char * /*devFun*/,
+		const char *devName, int /*threadLimit*/, uint3 * /*tid*/, uint3 * /*bid*/,
+		dim3 * /*bDim*/, dim3 * /*gDim*/)
+{
+    gmac::util::Logger::TRACE("CUDA Function");
+	ModuleDescriptor *mod = (ModuleDescriptor *)fatCubinHandle;
+	gmac::util::Logger::ASSERTION(mod != NULL);
+	gmac::enterGmac();
+    KernelDescriptor k = KernelDescriptor(devName, (gmacKernel_t) hostFun);
+    mod->add(k);
+	gmac::exitGmac();
+}
+
+GMAC_API void __cudaRegisterVar(void **fatCubinHandle, char *hostVar,
+		char * /*deviceAddress*/, const char *deviceName, int /*ext*/, int /*size*/,
+		int constant, int /*global*/)
+{
+    gmac::util::Logger::TRACE("CUDA Variable %s", deviceName);
+	ModuleDescriptor *mod = (ModuleDescriptor *)fatCubinHandle;
+	gmac::util::Logger::ASSERTION(mod != NULL);
+	gmac::enterGmac();
+    VariableDescriptor v = VariableDescriptor(deviceName, hostVar, bool(constant != 0));
+    mod->add(v);
+	gmac::exitGmac();
+}
+
+GMAC_API void __cudaRegisterTexture(void **fatCubinHandle, const struct textureReference *hostVar,
+		const void ** /*deviceAddress*/, const char *deviceName, int /*dim*/, int /*norm*/, int /*ext*/)
+{
+    gmac::util::Logger::TRACE("CUDA Texture");
+	ModuleDescriptor *mod = (ModuleDescriptor *)fatCubinHandle;
+	gmac::util::Logger::ASSERTION(mod != NULL);
+	gmac::enterGmac();
+    TextureDescriptor t = TextureDescriptor(deviceName, hostVar);
+	mod->add(t);
+	gmac::exitGmac();
+}
+
+GMAC_API void __cudaRegisterShared(void ** /*fatCubinHandle*/, void ** /*devicePtr*/)
+{
+}
+
+GMAC_API void __cudaRegisterSharedVar(void ** /*fatCubinHandle*/, void ** /*devicePtr*/,
+		size_t /*size*/, size_t /*alignment*/, int /*storage*/)
+{
+}
+
+GMAC_API cudaError_t CUDARTAPI cudaConfigureCall(dim3 gridDim, dim3 blockDim,
+		size_t sharedMem, cudaStream_t tokens)
+{
+	gmac::enterGmac();
+    Mode &mode = gmac::cuda::Mode::current();
+	mode.call(gridDim, blockDim, sharedMem, tokens);
+	gmac::exitGmac();
+	return cudaSuccess;
+}
+
+GMAC_API cudaError_t CUDARTAPI cudaSetupArgument(const void *arg, size_t count, size_t offset)
+{
+	gmac::enterGmac();
+    Mode &mode = gmac::cuda::Mode::current();
+	mode.argument(arg, count, (off_t)offset);
+	gmac::exitGmac();
+	return cudaSuccess;
+}
+
+extern gmacError_t gmacLaunch(gmacKernel_t k);
+GMAC_API cudaError_t CUDARTAPI cudaLaunch(gmacKernel_t k)
+{
+	gmacError_t ret = gmacLaunch(k);
+    assert(ret == gmacSuccess);
+	return cudaSuccess;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+
---
+++ src/util/posix/dbc/Lock.cpp
@@ -0,0 +1,112 @@
+#include "Lock.h"
+
+namespace gmac { namespace util { namespace __dbc {
+
+Lock::Lock(const char *name) :
+    __impl::Lock(name),
+    locked_(false),
+    owner_(NULL)
+{
+    pthread_mutex_init(&internal_, NULL);
+}
+
+Lock::~Lock()
+{
+    pthread_mutex_destroy(&internal_);
+}
+
+void Lock::lock() const
+{
+    //pthread_mutex_lock(&internal_);
+    //REQUIRES(owner_ != pthread_self());
+    //pthread_mutex_unlock(&internal_);
+
+    __impl::Lock::lock();
+
+    pthread_mutex_lock(&internal_);
+    ENSURES(owner_ == 0);
+    ENSURES(locked_ == false);
+    locked_ = true;
+    owner_ = pthread_self();
+    pthread_mutex_unlock(&internal_);
+}
+
+void Lock::unlock() const
+{
+    pthread_mutex_lock(&internal_);
+    REQUIRES(locked_ == true);
+    REQUIRES(owner_ == pthread_self());
+    owner_ = NULL;
+    locked_ = false;
+
+    __impl::Lock::unlock();
+
+    pthread_mutex_unlock(&internal_);
+}
+
+RWLock::RWLock(const char *name) :
+    __impl::RWLock(name),
+    state_(Idle),
+    writer_(NULL)
+{
+    pthread_mutex_init(&internal_, NULL);
+}
+
+RWLock::~RWLock()
+{
+    pthread_mutex_destroy(&internal_);
+}
+
+void RWLock::lockRead() const
+{
+    pthread_mutex_lock(&internal_);
+    REQUIRES(readers_.find(pthread_self()) == readers_.end());
+    pthread_mutex_unlock(&internal_);
+
+    __impl::RWLock::lockRead();
+
+    pthread_mutex_lock(&internal_);
+    ENSURES(state_ == Idle || state_ == Read);
+    state_ = Read;
+    readers_.insert(pthread_self());
+    pthread_mutex_unlock(&internal_);
+}
+
+void RWLock::lockWrite() const
+{
+    pthread_mutex_lock(&internal_);
+    REQUIRES(readers_.find(pthread_self()) == readers_.end());
+    REQUIRES(writer_ != pthread_self());
+    pthread_mutex_unlock(&internal_);
+
+    __impl::RWLock::lockWrite();
+
+    pthread_mutex_lock(&internal_);
+    ENSURES(readers_.empty() == true);
+    ENSURES(state_ == Idle);
+    state_ = Write;
+    writer_ = pthread_self();
+    pthread_mutex_unlock(&internal_);
+}
+
+void RWLock::unlock() const
+{
+    pthread_mutex_lock(&internal_);
+    if(writer_ == pthread_self()) {
+        REQUIRES(readers_.empty() == true);
+        REQUIRES(state_ == Write);
+        state_ = Idle;
+        writer_ = NULL;
+    }
+    else {
+        REQUIRES(readers_.erase(pthread_self()) == 1);
+        REQUIRES(state_ == Read);
+        if(readers_.empty() == true) state_ = Idle;
+    }
+
+    __impl::RWLock::unlock();
+
+    pthread_mutex_unlock(&internal_);
+}
+
+}}}
