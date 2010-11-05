29d3c1e2473d56369b4ef8cf048cacceb9e542ca
Isaac Gelado <igelado@ac.upc.edu>
2010-11-05 23:42:12 +0100

    Added one more test to buddy

--- tests/unit/core/allocator/Buddy.cpp
+++ tests/unit/core/allocator/Buddy.cpp
@@ -2,6 +2,7 @@
 
 #include "gtest/gtest.h"
 
+#include <cstdlib>
 #include <list>
 
 using gmac::core::allocator::Buddy;
@@ -9,6 +10,8 @@ using gmac::core::allocator::Buddy;
 class BuddyTest : public testing::Test {
 public:
     static const size_t Size_ = 8 * 1024 * 1024;
+    static unsigned long Base_;
+    static void *BasePtr_;
 
     static void SetUpTestCase() {
     }
@@ -18,16 +21,19 @@ public:
 };
 
 
+unsigned long BuddyTest::Base_ = 0x1000;
+void *BuddyTest::BasePtr_ = (void *)Base_;
+
 TEST_F(BuddyTest, SimpleAllocations) {
-    Buddy buddy((void *)0x1000, Size_);
-    ASSERT_TRUE(buddy.addr() == (void *)0x1000);
+    Buddy buddy(BasePtr_, Size_);
+    ASSERT_TRUE(buddy.addr() == BasePtr_);
 
     std::list<void *> allocs;
     void *ret = 0;
     for(unsigned n = 2; n < 32; n = n * 2) {
         size_t size = Size_ / n;
         ret = buddy.get(size);
-        ASSERT_EQ((unsigned long)0x1000, (unsigned long)ret % (Size_ / n));
+        ASSERT_EQ(Base_, (unsigned long)ret % (Size_ / n));
         ASSERT_EQ(size, (Size_ / n));
         allocs.push_back(ret);
     }
@@ -49,4 +55,40 @@ TEST_F(BuddyTest, SimpleAllocations) {
     ret = buddy.get(size);
     ASSERT_TRUE(ret != NULL);
     ASSERT_EQ(size, Size_ / 2);
+}
+
+TEST_F(BuddyTest, RandomAllocations) {
+    const int Allocations = 1024;
+    const size_t MinMemory = 4 * 4096;
+    size_t freeMemory = Size_;
+    Buddy buddy(BasePtr_, Size_);
+    ASSERT_TRUE(buddy.addr() == BasePtr_);
+
+    typedef std::map<void *, size_t> AllocMap;
+    AllocMap map;
+    srand(time(NULL));
+    for(int i = 0; i < Allocations; i++) {
+        // Generate a random size to be allocated
+        size_t s = 0;
+        while(s == 0) s = size_t(freeMemory * rand() / (RAND_MAX + 1.0));
+
+        // Perform the allocation
+        size_t check = s;
+        void *addr = buddy.get(s);
+        if(addr == NULL) {--i; continue; } // Too much memory, try again
+        ASSERT_GE(s, check);
+        std::pair<AllocMap::iterator, bool> p = map.insert(AllocMap::value_type(addr, s));
+        ASSERT_EQ(true, p.second);
+        freeMemory -= s;
+
+        if(freeMemory > MinMemory) continue;
+
+        int n = int(map.size() * rand() / (RAND_MAX + 1.0));
+        AllocMap::iterator i = map.begin();
+        for(; n > 0; n--, i++);
+        buddy.put(i->first, i->second);
+        freeMemory += i->second;
+        map.erase(i);
+    }
+
 }
