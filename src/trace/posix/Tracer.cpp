426994b6b80e50f4af9bb76d333f0df59236be99
Isaac Gelado <Isaac Gelado@1083b7e4-13de-4bf8-a848-09d336b14c3b>
2010-11-17 06:05:20 +0000

    Added missing POSIX tracer files

---
+++ src/trace/posix/Tracer.cpp
@@ -0,0 +1,13 @@
+#include "trace/Tracer.h"
+
+#include <sys/time.h>
+
+namespace gmac { namespace trace {
+uint64_t Tracer::timeMark() const
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	uint64_t tm = tv.tv_usec + 1000000 * tv.tv_sec;
+	return tm;
+}
+}}
