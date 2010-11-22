0e273e947f604565742252c3b1b67b3cad5b4a5c
Isaac Gelado <Isaac Gelado@1083b7e4-13de-4bf8-a848-09d336b14c3b>
2010-11-22 06:08:34 +0000

    Fixed bad Object::memoryOp -- gmacFileVecAdd working

--- src/memory/Object.cpp
+++ src/memory/Object.cpp
@@ -20,15 +20,24 @@ gmacError_t Object::memoryOp(Protocol::MemoryOp op, core::IOBuffer &buffer, size
 							 unsigned bufferOffset, unsigned objectOffset) const
 {
 	gmacError_t ret = gmacSuccess;
-	unsigned blockOffset = objectOffset;
-	BlockMap::const_iterator i;
-	for(i = blocks_.begin(); i != blocks_.end(); i++) {
+	// Skip blocks at the begining of the object until we reach 
+    // the block where the operation has to be applied for the
+    // first time
+	BlockMap::const_iterator i = blocks_.begin();
+    while(objectOffset >= i->second->size()) {
+        objectOffset -= unsigned(i->second->size());
+        i++;
+        ASSERTION(i != blocks_.end());
+    }
+    unsigned blockOffset = objectOffset;
+	for(; i != blocks_.end(); i++) {
 		size_t blockSize = size - blockOffset;
 		blockSize = (blockSize < i->second->size()) ? blockSize : i->second->size();
 		ret = i->second->memoryOp(op, buffer, blockSize, bufferOffset, blockOffset);
 		blockOffset = 0;
 		bufferOffset += unsigned(i->second->size());
 		size -= blockSize;
+        if(size == 0) break;
 	}
 	return ret;
 }
