2e3aa9c0c080df798b70c8cb8f45aa5dab8411e0
Isaac Gelado <Isaac Gelado@1083b7e4-13de-4bf8-a848-09d336b14c3b>
2010-11-23 23:41:19 +0000

    Added doxygen documentation for Object

--- src/memory/Object.h
+++ src/memory/Object.h
@@ -53,51 +53,201 @@ namespace memory {
 
 class GMAC_LOCAL Object: protected gmac::util::RWLock, public util::Reference {
 protected:
+    //! Object host memory address
     uint8_t *addr_;
+
+    //! Object size in bytes
     size_t size_;
 
+    //! Mark the object as valid
 	bool valid_;
 
 	typedef std::map<uint8_t *, Block *> BlockMap;
+    //! Collection of blocks forming the object
 	BlockMap blocks_;
 
+    //! Returns the block corresponding to a given offest from the begining of the object
+    /*!
+        \param objectOffset Offset (in bytes) from the begining of the object where the block is located
+        \return Constant iterator pointing to the block
+    */
     BlockMap::const_iterator firstBlock(unsigned &objectOffset) const;
 
+    //! Execute a coherence operation over all the blocks of the object
+    /*!
+        \param op Coherence operation to be performed
+        \return Error code
+        \sa __impl::memory::Block::toHost
+        \sa __impl::memory::Block::toDevice
+    */
 	gmacError_t coherenceOp(Protocol::CoherenceOp op) const;
+
+    //! Execute a memory operation involving an I/O buffer over all the blocks of the object
+    /*!
+        \param op Memory operation to be executed
+        \param buffer I/O buffer used in the memory operation
+        \param size Size (in bytes) of the memory operation
+        \param bufferOffset Offset (in bytes) from the begining of the buffer to start performing the operation
+        \param objectOffset Offset (in bytes) from the beginning of the block to start performing the operation
+        \return Error code
+        \sa __impl::memory::Block::copyToHost(core::IOBuffer &, size_t, unsigned, unsigned) const
+        \sa __impl::memory::Block::copyToDevice(core::IOBuffer &, size_t, unsigned, unsigned) const
+        \sa __impl::memory::Block::copyFromHost(core::IOBuffer &, size_t, unsigned, unsigned) const
+        \sa __impl::memory::Block::copyFromDevice(core::IOBuffer &, size_t, unsigned, unsigned) const
+    */
 	gmacError_t memoryOp(Protocol::MemoryOp op, core::IOBuffer &buffer, size_t size, 
 		unsigned bufferOffset, unsigned objectOffset) const;
 
+    //! Default constructor
+    /*!
+        \param addr Host memory address where the object begins
+        \param size Size (in bytes) of the memory object
+    */
 	Object(void *addr, size_t size);
+
+    //! Default destructor
 	virtual ~Object();
 public:
-
+    //! Get the starting host memory address of the object
+    /*!
+        \return Starting host memory address of the object
+    */
     uint8_t *addr() const;
+
+    //! Get the ending host memory address of the object
+    /*!
+        \return Ending host memory address of the object
+    */
     uint8_t *end() const;
+
+    //! Get the size (in bytes) of the object
+    /*!
+        \return Size (in bytes) of the object
+    */
     size_t size() const;
+
+    //! Check whether the object is valid or not
+    /*!
+        \return Validity of the object
+    */
 	bool valid() const;
 
+    //! Get the device memory address where a host memory address from the object is mapped
+    /*!
+        \param addr Host memory address within the object
+        \return Device memory address within the object
+    */
     virtual void *deviceAddr(const void *addr) const = 0;
+
+    //! Get the owner of the object
+    /*!
+        \return The owner of the object
+    */
 	virtual core::Mode &owner(const void *addr) const = 0;
     
+    //! Add a new owner to the object
+    /*!
+        \param The new owner of the mode
+        \return Wether it was possible to add the owner or not
+    */
 	virtual bool addOwner(core::Mode &owner) = 0;
+
+    //! Remove an owner from the object
+    /*!
+        \param owner The owner to be removed
+    */
 	virtual void removeOwner(const core::Mode &owner) = 0;
 
+    //! Acquire the ownership of the object for the CPU
+    /*!
+        \return Error code
+    */
 	gmacError_t acquire() const;
+
+    //! Ensures that the object host memory contains an updated copy of the data
+    /*!
+        \return Error code
+    */
 	gmacError_t toHost() const;
+
+    //! Ensures that the object device memory contains an updated copy of the data
+    /*!
+        \return Error code
+    */
 	gmacError_t toDevice() const;
 
+    //! Signal handler for faults caused due to memory reads
+    /*!
+        \param addr Host memory address causing the fault
+        \return Error code
+    */
 	gmacError_t signalRead(void *addr) const;
+
+    //! Signal handler for faults caused due to memory writes
+    /*!
+        \param addr Host memory address causing the fault
+        \return Error code
+    */
 	gmacError_t signalWrite(void *addr) const;
 
+    //! Copies the data from the object to an I/O buffer
+    /*!
+        \param buffer I/O buffer where the data will be copied
+        \param size Size (in bytes) of the data to be copied
+        \param bufferOffset Offset (in bytes) from the begining of the I/O buffer to start copying the data to
+        \param objectOffset Offset (in bytes) from the begining og the object to start copying data from
+        \return Error code
+    */
 	gmacError_t copyToBuffer(core::IOBuffer &buffer, size_t size, 
-		unsigned bufferOffset = 0, unsigned objectOffset = 0) const;	
+		unsigned bufferOffset = 0, unsigned objectOffset = 0) const;
+
+    //! Copies the data from an I/O buffer to the object
+    /*!
+        \param buffer I/O buffer where the data will be copied from
+        \param size Size (in bytes) of the data to be copied
+        \param bufferOffset Offset (in bytes) from the begining of the I/O buffer to start copying the data from
+        \param objectOffset Offset (in bytes) from the begining og the object to start copying data to
+        \return Error code
+    */
 	gmacError_t copyFromBuffer(core::IOBuffer &buffer, size_t size, 
 		unsigned bufferOffset = 0, unsigned objectOffset = 0) const;
 
+    //! Initializes a memory range within the object to a specific value
+    /*!
+        \param addr Host memory address within the object to be initialized
+        \param v Value to initialize the memory to
+        \param size Size (in bytes) of the memory region to be initialized
+        \return Error code
+    */
     gmacError_t memset(void *addr, int v, size_t size) const;
+
+    //! Copy data from host memory to the object
+    /*!
+        \param dst Destination host memory address within the object to copy the data to
+        \param src Source host memory address to copy the data from
+        \param size Size (in bytes) of the data to be copied
+        \return Error code
+    */
     gmacError_t memcpyFromMemory(void *dst, const void *src, size_t size) const;
+
+    //! Copy data from an object to the object
+    /*!
+        \param dst Destination host memory address within the object to copy the data from
+        \param src Source object to copy the data from
+        \param size Size (in bytes) of the data to be copied
+        \param objectOffset Offset (in bytes) within the source object to copy the data from
+        \return Error code
+    */
     gmacError_t memcpyFromObject(void *dst, const Object &src, 
         size_t size, unsigned objectOffset = 0) const;
+
+    //! Copy data from the object to host memory 
+    /*!
+        \param dst Destination host memory address to copy the data to
+        \param src Source host memory address within the object to copy the data from
+        \param size Size (in bytes) of the data to be copied
+        \return Error code
+    */
     gmacError_t memcpyToMemory(void *dst, const void *src, size_t size) const;
 };
 
