f48175778899305c61071030b3e907f93cdaa02b
Javier Cabezas <Javier Cabezas@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-01-12 17:06:20 +0000

    Fixed attribute naming in cuda::Kernel

--- src/api/cuda/Kernel.cpp
+++ src/api/cuda/Kernel.cpp
@@ -10,10 +10,10 @@ namespace __impl { namespace cuda {
 Kernel::Kernel(const core::KernelDescriptor & k, CUmodule mod) :
     core::Kernel(k)
 {
-    CUresult ret = cuModuleGetFunction(&_f, mod, name_);
+    CUresult ret = cuModuleGetFunction(&f_, mod, name_);
     //! \todo Calculate this dynamically
 #if CUDA_VERSION >= 3000 && LINUX
-    ret = cuFuncSetCacheConfig(_f, CU_FUNC_CACHE_PREFER_L1);
+    ret = cuFuncSetCacheConfig(f_, CU_FUNC_CACHE_PREFER_L1);
     ASSERTION(ret == CUDA_SUCCESS);
 #endif
     ASSERTION(ret == CUDA_SUCCESS);
@@ -30,27 +30,27 @@ Kernel::launch(core::KernelConfig & _c)
 
 KernelConfig::KernelConfig(const KernelConfig & c) :
     core::KernelConfig(c),
-    _grid(c._grid),
-    _block(c._block),
-    _shared(c._shared),
-    _stream(c._stream)
+    grid_(c.grid_),
+    block_(c.block_),
+    shared_(c.shared_),
+    stream_(c.stream_)
 {
 }
 
 KernelConfig::KernelConfig(dim3 grid, dim3 block, size_t shared, cudaStream_t /*tokens*/) :
     core::KernelConfig(),
-    _grid(grid),
-    _block(block),
-    _shared(shared),
-    _stream(NULL)
+    grid_(grid),
+    block_(block),
+    shared_(shared),
+    stream_(NULL)
 {
 }
 
 KernelLaunch::KernelLaunch(const Kernel & k, const KernelConfig & c) :
     core::KernelLaunch(),
     cuda::KernelConfig(c),
-    _kernel(k),
-    _f(k._f)
+    kernel_(k),
+    f_(k.f_)
 {
 }
 
@@ -58,30 +58,30 @@ gmacError_t
 KernelLaunch::execute()
 {
 	// Set-up parameters
-    CUresult ret = cuParamSetv(_f, 0, argsArray(), argsSize());
+    CUresult ret = cuParamSetv(f_, 0, argsArray(), argsSize());
     CFATAL(ret == CUDA_SUCCESS, "CUDA Error setting parameters: %d", ret);
-    ret = cuParamSetSize(_f, argsSize());
+    ret = cuParamSetSize(f_, argsSize());
 	CFATAL(ret == CUDA_SUCCESS);
 
 #if 0
 	// Set-up textures
 	Textures::const_iterator t;
 	for(t = textures_.begin(); t != textures_.end(); t++) {
-		cuParamSetTexRef(_f, CU_PARAM_TR_DEFAULT, *(*t));
+		cuParamSetTexRef(f_, CU_PARAM_TR_DEFAULT, *(*t));
 	}
 #endif
 
 	// Set-up shared size
-	if((ret = cuFuncSetSharedSize(_f, (unsigned int)shared())) != CUDA_SUCCESS) {
+	if((ret = cuFuncSetSharedSize(f_, (unsigned int)shared())) != CUDA_SUCCESS) {
         goto exit;
 	}
 
-	if((ret = cuFuncSetBlockShape(_f, block().x, block().y, block().z))
+	if((ret = cuFuncSetBlockShape(f_, block().x, block().y, block().z))
 			!= CUDA_SUCCESS) {
         goto exit;
 	}
 
-	ret = cuLaunchGridAsync(_f, grid().x, grid().y, _stream);
+	ret = cuLaunchGridAsync(f_, grid().x, grid().y, stream_);
 
 exit:
     return Accelerator::error(ret);
