efba1ba85d4c840b884f702e004d4ad53b0e57b8
dongshaokai <dongshaokai@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-01-16 10:23:01 +0000

    Added tests for core/Process

---
+++ tests/unit/core/Process.cpp
@@ -0,0 +1,75 @@
+#include "unit/init.h"
+#include "unit/core/Process.h"
+#include  "core/Mode.h"
+
+
+
+
+
+using gmac::core::Mode;
+using gmac::core::Accelerator;
+using gmac::core::ModeMap;
+using gmac::core::Process;
+using gmac::core::QueueMap;
+using namespace gmac::util;
+using gmac::core::ThreadQueue;
+
+
+Mode *ProcessTest::Mode_ = NULL;
+
+void ProcessTest::SetUpTestCase() {
+	InitProcess();
+	if(Mode_ != NULL) return;
+	Mode_ = dynamic_cast<Mode *>(Process::getInstance().createMode(0));
+	ASSERT_TRUE(Mode_ != NULL);
+	Mode_->initThread();
+}
+
+
+TEST( ModeMapTest,MemberFunc){
+
+	InitProcess();
+	Mode *mode_= dynamic_cast<Mode *>(Process::getInstance().createMode(0));
+	ASSERT_TRUE(mode_ != NULL);
+	//mode_->initThread();
+	Accelerator &acc_=GetAccelerator();
+	ASSERT_TRUE(&acc_ != NULL);
+	ModeMap mm;
+//	ASSERT_TRUE(&mm != NULL);
+	
+	typedef std::map<Mode *, Accelerator *> Parent;
+	typedef Parent::iterator iterator;
+	std::pair<iterator, bool> ib=mm.insert(mode_,&acc_);
+	ASSERT_TRUE(ib.second);
+	ASSERT_TRUE(mm.remove(*mode_)==1);
+	ASSERT_TRUE(mm.remove(*mode_)==0);
+
+
+}
+
+TEST_F(ProcessTest,QueueMapTestCase){
+	unsigned long id=GetThreadId();
+	ASSERT_TRUE(id != 0);
+
+	ThreadQueue tq;
+	ASSERT_TRUE(tq.queue != NULL);
+
+	typedef std::map<THREAD_T, ThreadQueue *> Parent;
+	typedef Parent::iterator iterator;
+	QueueMap qm;
+	//ASSERT_TRUE(&qm != NULL);
+	std::pair<iterator, bool> ib=qm.insert(id, &tq);
+	ASSERT_TRUE(ib.second);
+
+	//cteate another Mode,because  want to test member  function  push()
+	// can not cteate it , bacause only one accelerator in current OS.
+	//Mode *mode_= dynamic_cast<Mode *>(Process::getInstance().createMode(1));
+	//ASSERT_TRUE(mode_ != NULL);
+
+	//qm.push(id,*mode_);
+
+	// 因为tq 如果在这释放的话，退出作用域时还会调用析构函数，释放，所以抛出异常
+	//ASSERT_ANY_THROW(qm.erase(id));
+	//qm.cleanup();
+
+}
