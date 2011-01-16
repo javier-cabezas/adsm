f49faf5da54253f1438024f23237f78b5587b815
dongshaokai <dongshaokai@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-01-16 09:34:01 +0000

    added test case for core/Context

--- tests/unit/core/init.cpp
+++ tests/unit/core/init.cpp
@@ -2,14 +2,20 @@
 #include "trace/Tracer.h"
 #include "core/Process.h"
 #include "core/Accelerator.h"
+#include "core/Context.h" //added 
 
 #include "gtest/gtest.h"
 
 using __impl::core::Accelerator;
 using __impl::core::Process;
+using __impl::core::Context; //added
+//using gmac::core::Context; 
+
 
 Accelerator *Accelerator_ = NULL;
+Context *Context_=NULL; //added
 static bool Trace_ = false;
+
 
 void InitTrace(void)
 {
@@ -37,10 +43,27 @@ void FiniAccelerator()
     Accelerator_ = NULL;
 }
 
+
+
+gmac::core::Context &GetContext()
+{
+    return *Context_;
+}
+
+void FiniContext()
+{
+    if( Context_==NULL) return;
+    delete Context_; 
+    Context_=NULL;
+    ASSERT_TRUE(Accelerator_ != NULL);
+    Accelerator_=NULL;
+}
+
 void InitProcess()
 {
     InitTrace();
     InitAccelerator();
+    InitContext(); //added 
 
     Process::create<Process>();
     Process &proc = Process::getInstance();
@@ -52,6 +75,10 @@ void FiniProcess()
 {
     ASSERT_TRUE(Accelerator_ != NULL);
     Accelerator_ = NULL;
+    ASSERT_TRUE(Context_ != NULL); //added
+    Context_=NULL;
+   
+
     Process::destroy();
     FiniTrace();
 }
