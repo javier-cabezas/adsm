574049c17281125af02aa9db4530d6a228afd231
dongshaokai <dongshaokai@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-01-23 07:58:33 +0000

    Added dbc check on generic core/IOBuffer methods lock() and unlock()

--- src/core/dbc/IOBuffer.cpp
+++ src/core/dbc/IOBuffer.cpp
@@ -16,12 +16,12 @@ void IOBuffer::lock()
 {
     __impl::core::IOBuffer::lock();
 
-    //thread_mutex_lock(&internal_);
-    //ENSURES(owner == 0);
-    //ENSURES(locked == false);
-    //locked_ = true;
-    //owner_ = pthread_self();
-    //pthread_mutex_unlock(&internal_);
+    pthread_mutex_lock(&internal_);
+    ENSURES(owner_ == 0);
+    ENSURES(locked_ == false);
+    locked_ = true;
+    owner_ = pthread_self();
+    pthread_mutex_unlock(&internal_);
 
 
 
@@ -30,15 +30,15 @@ void IOBuffer::lock()
 void IOBuffer::unlock()
 {
  
-     // pthread_mutex_lock(&internal_);
-     // REQUIRES(locked_ == true);
-     // REQUIRES(owner_ == pthread_self());
-     // owner_ = NULL;
-     // locked_ = false;
+      pthread_mutex_lock(&internal_);
+      REQUIRES(locked_ == true);
+      REQUIRES(owner_ == pthread_self());
+      owner_ = NULL;
+      locked_ = false;
 
       __impl::core::IOBuffer::unlock();  
 
-     // pthread_mutex_unlock(&internal_);
+      pthread_mutex_unlock(&internal_);
 
 }
 
