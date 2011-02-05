545c1faf704b87f8fbe6b195fad1e86fb9b18060
Javier Cabezas <Javier Cabezas@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-02-05 07:03:06 +0000

    Proper OpenCL device initialization

--- src/api/opencl/opencl.cpp
+++ src/api/opencl/opencl.cpp
@@ -15,31 +15,34 @@ void apiInit(void)
     if(initialized) FATAL("GMAC double initialization not allowed");
 
     TRACE(GLOBAL, "Initializing OpenCL API");
-    int platformVectorSize = 32;
     cl_uint platformSize = 0;
-    cl_platform_id * platforms = new cl_platform_id[platformVectorSize];
-
-    cl_int ret = clGetPlatformIDs(platformVectorSize, platforms, &platformSize);
+    cl_int ret;
+    ret = clGetPlatformIDs(0, NULL, &platformSize);
+    ASSERTION(ret == CL_SUCCESS);
+    cl_platform_id * platforms = new cl_platform_id[platformSize];
+    ret = clGetPlatformIDs(platformSize, platforms, NULL);
     ASSERTION(ret == CL_SUCCESS);
     TRACE(GLOBAL, "Found %d OpenCL platforms", platformSize);
 
-    int deviceVectorSize = 32;
     unsigned n = 0;
-    cl_device_id *devices = new cl_device_id[deviceVectorSize];  
-    cl_uint size = 0;
     for(unsigned i = 0; i < platformSize; i++) {
+        cl_uint deviceSize = 0;
         ret = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU,
-            deviceVectorSize, devices, &size);
+            0, NULL, &deviceSize);
         ASSERTION(ret == CL_SUCCESS);
-        TRACE(GLOBAL, "Found %d OpenCL devices in platform %d", size, i);
-        for(unsigned j = 0; j < size; j++) {
+        cl_device_id *devices = new cl_device_id[deviceSize];  
+        ret = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU,
+            deviceSize, devices, NULL);
+        ASSERTION(ret == CL_SUCCESS);
+        TRACE(GLOBAL, "Found %d OpenCL devices in platform %d", deviceSize, i);
+        for(unsigned j = 0; j < deviceSize; j++) {
             gmac::opencl::Accelerator *acc = new gmac::opencl::Accelerator(n++, platforms[i], devices[j]);
             proc.addAccelerator(*acc);
             // Nedded for OpenCL code compilation
             __impl::opencl::Accelerator::addAccelerator(*acc);
         }
+        delete[] devices;
     }
-    delete[] devices;
     delete[] platforms;
     initialized = true;
 }
