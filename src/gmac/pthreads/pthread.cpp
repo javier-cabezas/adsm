9431843531a53620eff76067c3303aa6482c0abe
Isaac Gelado <Isaac Gelado@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-02-11 05:34:20 +0000

    Fixed bad thread handling on OpenCL-created threads for AMD

--- src/gmac/pthreads/pthread.cpp
+++ src/gmac/pthreads/pthread.cpp
@@ -25,6 +25,7 @@ static void __attribute__((destructor())) gmacPthreadFini(void)
 struct gmac_thread_t {
 	void *(*__start_routine)(void *);
 	void *__arg;
+    bool __do_exit;
 };
 
 using __impl::core::Process;
@@ -34,12 +35,13 @@ static void *gmac_pthread(void *arg)
     gmac::trace::StartThread("CPU");
 	gmac::enterGmac();
 	gmac_thread_t *gthread = (gmac_thread_t *)arg;
+    bool do_exit = gthread->__do_exit;
     Process &proc = Process::getInstance();
     proc.initThread();
     gmac::trace::SetThreadState(gmac::trace::Running);
-	gmac::exitGmac();
+	if(do_exit) gmac::exitGmac();
 	void *ret = gthread->__start_routine(gthread->__arg);
-	gmac::enterGmac();
+	if(do_exit) gmac::enterGmac();
     // Modes and Contexts already destroyed in Process destructor
     proc.finiThread();
 	free(gthread);
@@ -53,16 +55,15 @@ int pthread_create(pthread_t *__restrict __newthread,
                    void *(*__start_routine)(void *),
                    void *__restrict __arg)
 {
-    if(gmac::inGmac() == 1)
-        return __pthread_create(__newthread, __attr, __start_routine, __arg);
-        
 	int ret = 0;
-	gmac::enterGmac();
+    bool externCall = gmac::inGmac() == 0;
+    if(externCall) gmac::enterGmac();
     TRACE(GLOBAL, "New POSIX thread");
 	gmac_thread_t *gthread = (gmac_thread_t *)malloc(sizeof(gmac_thread_t));
 	gthread->__start_routine = __start_routine;
 	gthread->__arg = __arg;
+    gthread->__do_exit = externCall;
 	ret = __pthread_create(__newthread, __attr, gmac_pthread, (void *)gthread);
-	gmac::exitGmac();
+	if(externCall) gmac::exitGmac();
 	return ret;
 }
