956f1d7642bef7269be0490bc13172d3510a2f3d
Isaac Gelado <Isaac Gelado@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-02-12 01:29:01 +0000

    Added gmacCompress to test set

--- tests/simple/gmac/cuda/gmacCompress.cu
+++ tests/simple/gmac/cuda/gmacCompress.cu
@@ -17,14 +17,14 @@ const char *widthStr = "GMAC_WIDTH";
 const char *heightStr = "GMAC_HEIGHT";
 const char *framesStr = "GMAC_FRAMES";
 
-const size_t widthDefault = 128;
-const size_t heightDefault = 128;
-const size_t framesDefault = 32;
+const unsigned widthDefault = 128;
+const unsigned heightDefault = 128;
+const unsigned framesDefault = 32;
 
-size_t width = 0;
-size_t height = 0;
-size_t frames = 0;
-const size_t blockSize = 16;
+unsigned width = 0;
+unsigned height = 0;
+unsigned frames = 0;
+const unsigned blockSize = 16;
 
 static float *quant_in, *idct_in;
 
@@ -33,9 +33,9 @@ static sem_t quant_data, idct_data;
 static sem_t quant_free, idct_free;
 
 
-void __randInit(float *a, size_t size)
+void __randInit(float *a, unsigned size)
 {
-	for(int i = 0; i < size; i++) {
+	for(unsigned i = 0; i < size; i++) {
 		a[i] = 10.0 * rand() / RAND_MAX;
 	}
 }
@@ -44,30 +44,47 @@ void *dct_thread(void *args)
 {
 	float *in, *out;
 	gmacError_t ret;
+    gmactime_t s, t;
 
+    getTime(&s);
 	ret = gmacMalloc((void **)&in, width * height * sizeof(float));
 	assert(ret == gmacSuccess);
 	ret = gmacMalloc((void **)&out, width * height * sizeof(float));
 	assert(ret == gmacSuccess);
+    getTime(&t);
+    printTime(&s, &t, "DCT:Alloc: ", "\n");
 
 	dim3 Db(blockSize, blockSize);
 	dim3 Dg(width / blockSize, height / blockSize);
 	if(width % blockSize) Dg.x++;
 	if(height % blockSize) Dg.y++;
 
-	for(int i = 0; i < frames; i++) {
+	for(unsigned i = 0; i < frames; i++) {
+        getTime(&s);
 		__randInit(in, width * height);
+        getTime(&t);
+        printTime(&s, &t, "DCT:Init: ", "\n");
+
+        getTime(&s);
 		dct<<<Dg, Db>>>(gmacPtr(out), gmacPtr(in), width, height);
 		ret = gmacThreadSynchronize();
 		assert(ret == gmacSuccess);
+        getTime(&t);
+        printTime(&s, &t, "DCT:Run: ", "\n");
 
+        getTime(&s);
 		sem_wait(&quant_free); /* Wait for quant to use its data */
 		gmacMemcpy(quant_in, out, width * height * sizeof(float));
 		sem_post(&quant_data); /* Notify to Quant that data is ready */
+        getTime(&t);
+        printTime(&s, &t, "DCT:Copy: ", "\n");
 	}
 
+    getTime(&s);
 	gmacFree(in);
 	gmacFree(out);
+    getTime(&t);
+    printTime(&s, &t, "DCT:Free: ", "\n");
 
 	return NULL;
 }
@@ -76,11 +93,15 @@ void *quant_thread(void *args)
 {
 	float *out;
 	gmacError_t ret;
+    gmactime_t s, t;
 
+    getTime(&s);
 	ret = gmacMalloc((void **)&quant_in, width * height * sizeof(float));
 	assert(ret == gmacSuccess);
 	ret = gmacMalloc((void **)&out, width * height * sizeof(float));
 	assert(ret == gmacSuccess);
+    getTime(&t);
+    printTime(&s, &t, "Quant:Alloc: ", "\n");
 
 	dim3 Db(blockSize, blockSize);
 	dim3 Dg(width / blockSize, height / blockSize);
@@ -89,20 +110,29 @@ void *quant_thread(void *args)
 
 	sem_post(&quant_free);
 
-	for(int i = 0; i < frames; i++) {
+	for(unsigned i = 0; i < frames; i++) {
+        getTime(&s);
 		sem_wait(&quant_data);	/* Wait for data to be processed */
 		quant<<<Dg, Db>>>(gmacPtr(quant_in), gmacPtr(out), width, height, 1e-6);
 		ret = gmacThreadSynchronize();
 		assert(ret == gmacSuccess);
+        getTime(&t);
+        printTime(&s, &t, "Quant:Run: " , "\n");
 		
+        getTime(&s);
 		sem_wait(&idct_free); /* Wait for IDCT to use its data */
 		gmacMemcpy(idct_in, out, width * height * sizeof(float));
 		sem_post(&quant_free); /* Notify to DCT that Quant is waiting for data */
 		sem_post(&idct_data); /* Nodify to IDCT that data is ready */
+        getTime(&t);
+        printTime(&s, &t, "Quant:Copy: ", "\n");
 	}
 
+    getTime(&s);
 	gmacFree(quant_in);
 	gmacFree(out);
+    getTime(&t);
+    printTime(&s, &t, "Quant:Free: ", "\n");
 
 	return NULL;
 }
@@ -111,11 +141,15 @@ void *idct_thread(void *args)
 {
 	float *out;
 	gmacError_t ret;
+    gmactime_t s, t;
 
+    getTime(&s);
 	ret = gmacMalloc((void **)&idct_in, width * height * sizeof(float));
 	assert(ret == gmacSuccess);
 	ret = gmacMalloc((void **)&out, width * height * sizeof(float));
 	assert(ret == gmacSuccess);
+    getTime(&t);
+    printTime(&s, &t, "IDCT:Alloc: ", "\n");
 
 	dim3 Db(blockSize, blockSize);
 	dim3 Dg(width / blockSize, height / blockSize);
@@ -124,18 +158,24 @@ void *idct_thread(void *args)
 
 	sem_post(&idct_free);
 
-	for(int i = 0; i < frames; i++) {
+	for(unsigned i = 0; i < frames; i++) {
+        getTime(&s);
 		sem_wait(&idct_data);
 		idct<<<Dg, Db>>>(gmacPtr(idct_in), gmacPtr(out), width, height);
 		ret = gmacThreadSynchronize();
 		assert(ret == gmacSuccess);
 
 		sem_post(&idct_free);
+        getTime(&t);
+        printTime(&s, &t, "IDCT:Run: ", "\n");
 	}
 
 
+    getTime(&s);
 	gmacFree(idct_in);
 	gmacFree(out);
+    getTime(&t);
+    printTime(&s, &t, "IDCT:Free: ", "\n");
 
 	return NULL;
 }
@@ -143,10 +183,10 @@ void *idct_thread(void *args)
 
 int main(int argc, char *argv[])
 {
-	struct timeval s,t;
-	setParam<size_t>(&width, widthStr, widthDefault);
-	setParam<size_t>(&height, heightStr, heightDefault);
-	setParam<size_t>(&frames, framesStr, framesDefault);
+	gmactime_t s,t;
+	setParam<unsigned>(&width, widthStr, widthDefault);
+	setParam<unsigned>(&height, heightStr, heightDefault);
+	setParam<unsigned>(&frames, framesStr, framesDefault);
 
 	sem_init(&quant_data, 0, 0); 
 	sem_init(&quant_free, 0, 0); 
@@ -155,7 +195,7 @@ int main(int argc, char *argv[])
 
 	srand(time(NULL));
 
-	gettimeofday(&s, NULL);
+	getTime(&s);
 
 	pthread_create(&dct_id, NULL, dct_thread, NULL);
 	pthread_create(&quant_id, NULL, quant_thread, NULL);
@@ -165,7 +205,7 @@ int main(int argc, char *argv[])
 	pthread_join(quant_id, NULL);
 	pthread_join(idct_id, NULL);
 
-	gettimeofday(&t, NULL);
+	getTime(&t);
 
 	printTime(&s, &t, "Total: ", "\n");
 
