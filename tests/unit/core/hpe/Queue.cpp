a7f5330b3c816388bde9de24d130e12fce2f6d89
Isaac Gelado <Isaac Gelado@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-04-11 16:19:39 +0000

    Fixed double-free on UnitTets

--- tests/unit/core/hpe/Queue.cpp
+++ tests/unit/core/hpe/Queue.cpp
@@ -12,20 +12,20 @@ using __impl::core::hpe::Queue;
 using __impl::core::hpe::Process;
 using __impl::core::hpe::ThreadQueue;
 
-Mode *QueueTest::Mode_=NULL;
+Mode *QueueTest::Mode_ = NULL;
 
 void QueueTest::SetUpTestCase()
 {
      InitProcess();
      if(Mode_ != NULL) return;
      Mode_ = dynamic_cast<Mode *>(Process::getInstance<Process &>().createMode(0));
-     ASSERT_TRUE(Mode_!=NULL);
+     ASSERT_TRUE(Mode_ != NULL);
      Mode_->initThread();
 }
 TEST_F(QueueTest,MemberFun)
 {
     ThreadQueue temp_; 
-    ASSERT_TRUE(temp_.queue!=NULL);
+    ASSERT_TRUE(temp_.queue != NULL);
     temp_.queue->push(Mode_);
     Mode *last = dynamic_cast<gmac::core::hpe::Mode *>(temp_.queue->pop());
     ASSERT_EQ(Mode_,last);
