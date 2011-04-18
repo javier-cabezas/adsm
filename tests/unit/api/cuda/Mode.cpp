5cbe099df217fc4ea4b3bb63f4b56ffefadf8748
Isaac Gelado <Isaac Gelado@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-04-18 16:00:45 +0000

    Fixed Unit Test compilation for CUDA

--- tests/unit/api/cuda/Mode.cpp
+++ tests/unit/api/cuda/Mode.cpp
@@ -7,7 +7,7 @@ using __impl::cuda::hpe::Mode;
 TEST_F(ModeTest, ModeHostMemory) {
 	Mode &mode = dynamic_cast<Mode &>(*Mode_);
     hostptr_t addr = NULL;
-    ASSERT_EQ(gmacSuccess, mode.hostAlloc(&addr, Size_));
+    ASSERT_EQ(gmacSuccess, mode.hostAlloc(addr, Size_));
     ASSERT_TRUE(addr != NULL);
 
     ASSERT_EQ(gmacSuccess, mode.hostFree(addr));
