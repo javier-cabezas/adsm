100df34dd2c2797dc58a19b52b70b5fa04335fbf
Isaac Gelado <Isaac Gelado@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-04-23 19:04:26 +0000

    Added initial test-case for GMAC/Lite and added skeleton for OpenCL API hooking

--- src/api/opencl/lite/opencl.cpp
+++ src/api/opencl/lite/opencl.cpp
@@ -17,7 +17,7 @@ SYM(cl_context, __opencl_clCreateContext,
         cl_int *);
 
 SYM(cl_context, __opencl_clCreateContextFromType,
-        const cl_context_properties,
+        const cl_context_properties *,
         cl_device_type,
         void (CL_CALLBACK *)(const char *, const void *, size_t, void *),
         void *,
@@ -25,7 +25,17 @@ SYM(cl_context, __opencl_clCreateContextFromType,
 
 SYM(cl_int, __opencl_clRetainContext, cl_context);
 
-SYM(cl_int, __opencl_clCReleaseContext, cl_context);
+SYM(cl_int, __opencl_clReleaseContext, cl_context);
+
+SYM(cl_command_queue, __opencl_clCreateCommandQueue,
+        cl_context,
+        cl_device_id,
+        cl_command_queue_properties,
+        cl_int *);
+
+SYM(cl_int, __opencl_clRetainCommandQueue, cl_command_queue);
+
+SYM(cl_int, __opencl_clReleaseCommandQueue, cl_command_queue);
 
 SYM(cl_int, __opencl_clEnqueueNDRangeKernel,
         cl_command_queue,
@@ -62,6 +72,13 @@ SYM(cl_int, __opencl_clFinish, cl_command_queue);
 static void openclInit()
 {
     LOAD_SYM(__opencl_clCreateContext, clCreateContext);
+    LOAD_SYM(__opencl_clCreateContextFromType, clCreateContextFromType);
+    LOAD_SYM(__opencl_clRetainContext, clRetainContext);
+    LOAD_SYM(__opencl_clReleaseContext, clReleaseContext);
+
+    LOAD_SYM(__opencl_clCreateCommandQueue, clCreateCommandQueue);
+    LOAD_SYM(__opencl_clRetainCommandQueue, clRetainCommandQueue);
+    LOAD_SYM(__opencl_clReleaseCommandQueue, clReleaseCommandQueue);
 }
 
 #ifdef __cplusplus
@@ -78,11 +95,75 @@ cl_context SYMBOL(clCreateContext)(
 {
     if(__opencl_clCreateContext == NULL) openclInit();
     cl_context ret = __opencl_clCreateContext(properties, num_devices, devices, pfn_notify, user_data, errcode_ret);
-    if(errcode_ret != CL_SUCCESS) return ret;
+    if(*errcode_ret != CL_SUCCESS) return ret;
 
     return ret;
 }
 
+cl_context SYMBOL(clCreateContextFromType)(
+        const cl_context_properties *properties,
+        cl_device_type device_type,
+        void (CL_CALLBACK *pfn_notify)(const char *, const void *, size_t, void *),
+        void *user_data,
+        cl_int *errcode_ret)
+{
+    if(__opencl_clCreateContext == NULL) openclInit();
+    cl_context ret = __opencl_clCreateContextFromType(properties, device_type, pfn_notify, user_data, errcode_ret);
+    if(*errcode_ret != CL_SUCCESS) return ret;
+    return ret;
+}
+        
+cl_int SYMBOL(clRetainContext)(cl_context context)
+{
+    if(__opencl_clRetainContext == NULL) openclInit();
+    cl_int ret = __opencl_clRetainContext(context);
+    if(ret != CL_SUCCESS) return ret;
+    return ret;
+}
+
+cl_int SYMBOL(clReleaseContext)(cl_context context)
+{
+    if(__opencl_clReleaseContext == NULL) openclInit();
+    cl_int ret = __opencl_clReleaseContext(context);
+    if(ret != CL_SUCCESS) return ret;
+    return ret;
+}
+
+cl_command_queue SYMBOL(clCreateCommandQueue)(
+        cl_context context,
+        cl_device_id device,
+        cl_command_queue_properties properties,
+        cl_int *errcode_ret)
+{
+    if(__opencl_clCreateCommandQueue == NULL) openclInit();
+    cl_command_queue ret = __opencl_clCreateCommandQueue(context, device, properties,  errcode_ret);
+    if(*errcode_ret != CL_SUCCESS) return ret;
+
+    return ret;
+}
+
+cl_int SYMBOL(clRetainCommandQueue)(cl_command_queue command_queue)
+{
+    if(__opencl_clRetainCommandQueue == NULL) openclInit();
+    cl_int ret = __opencl_clRetainCommandQueue(command_queue);
+    if(ret != CL_SUCCESS) return ret;
+
+    return ret;
+}
+
+cl_int SYMBOL(clReleaseCommandQueue)(cl_command_queue command_queue)
+{
+    if(__opencl_clReleaseCommandQueue == NULL) openclInit();
+    cl_int ret = __opencl_clRetainCommandQueue(command_queue);
+    if(ret != CL_SUCCESS) return ret;
+
+    return ret;
+}
+
+static void CONSTRUCTOR init()
+{
+    fprintf(stderr,"Init GMAC/Lite\n");
+}
 
 #ifdef __cplusplus
 }
