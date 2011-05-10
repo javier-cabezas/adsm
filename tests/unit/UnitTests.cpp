69abc61b0f292ef52f006af533e6baf82dd249bf
Isaac Gelado <Isaac Gelado@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-05-10 14:13:53 +0000

    Fixed compilation error in Unit Tests

--- tests/unit/UnitTests.cpp
+++ tests/unit/UnitTests.cpp
@@ -3,7 +3,8 @@
 int main(int argc, char *argv[])
 {
     testing::InitGoogleTest(&argc, argv);
-    RUN_ALL_TESTS();
+    int ret = RUN_ALL_TESTS();
+    if(ret != 0) fprintf(stderr,"Some Tests were wrong\n");
     /* We always return 0 to let hudson continue executing tests */
     return 0;
 }
