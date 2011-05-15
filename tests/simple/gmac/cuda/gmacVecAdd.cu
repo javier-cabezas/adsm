394d5363a51a011a32b2abc64cff2c7ea1fd8cbd
Isaac Gelado <Isaac Gelado@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-05-15 05:34:27 +0000

    Fixed test compilation on Windows

--- tests/simple/gmac/cuda/gmacVecAdd.cu
+++ tests/simple/gmac/cuda/gmacVecAdd.cu
@@ -57,8 +57,8 @@ int main(int argc, char *argv[])
     getTime(&s);
     //randInit(a, vecSize);
     //randInit(b, vecSize);
-    init(a, vecSize, 1.f);
-    init(b, vecSize, 1.f);
+    init(a, int(vecSize), 1.f);
+    init(b, int(vecSize), 1.f);
     getTime(&t);
     printTime(&s, &t, "Init: ", "\n");
 
