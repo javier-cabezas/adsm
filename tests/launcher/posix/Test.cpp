572c8082d7df98b26e48274a90e76d15c4e8a954
Javier Cabezas <Javier Cabezas@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-05-17 19:51:58 +0000

    Remove debug printf

--- tests/launcher/posix/Test.cpp
+++ tests/launcher/posix/Test.cpp
@@ -69,7 +69,6 @@ Test::TestCase::run(const std::string &exec)
         ::wait(&status_); 
         ::getTime(&end);
         setElapsedTime((end.sec + double(end.usec) / 1000000.0) - (start.sec + double(start.usec) / 1000000.0));
-        std::cout << "MIERDA: " << time_ << std::endl;
         fclose(file);
     }
     return stats;
