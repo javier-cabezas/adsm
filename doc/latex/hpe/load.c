5b4935829e78f65e8bded6ede29d9c42b274a378
Javier Cabezas <Javier Cabezas@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-05-27 06:18:54 +0000

    Updated code and API of the user manual

--- doc/latex/hpe/load.c
+++ doc/latex/hpe/load.c
@@ -3,5 +3,5 @@
 . . .
 
     oclError_t error_code;
-    error_code = __oclPrepareCLCodeFromFile(kernel_file);
+    error_code = oclCompileSourceFile(kernel_file);
     if(error != oclSuccess) error(error_code);
