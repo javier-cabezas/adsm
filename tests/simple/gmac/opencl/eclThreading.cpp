478915aa6cd91f7c51380fe61ad9dc1e15f70e6f
Javier Cabezas <Javier Cabezas@20123256-afb6-45ad-a535-e83ccdd26e71>
2011-06-04 04:41:36 +0000

    Added threading benchmark

---
+++ tests/simple/gmac/opencl/eclThreading.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <cstring>
+#include <gmac/opencl.h>
+
+#include "barrier.h"
+#include "utils.h"
+
+#define THREADS 16
+#define ITERATIONS 100
+
+const unsigned size = 1024 * 1024;
+const unsigned totalSize = THREADS * size;
+
+unsigned *ptr;
+barrier_t barrier;
+
+const char *kernel = "\
+__kernel void inc(__global unsigned *a, unsigned size)\
+{\
+    unsigned i = get_global_id(0);\
+    if(i >= size) return;\
+\
+	a[i] += 1;\
+}\
+";
+
+unsigned sentinel = 0;
+
+void *func(void *p)
+{
+    unsigned id = *(unsigned *) p;
+    unsigned offset = id * size;
+
+    barrier_wait(&barrier);
+
+    for (unsigned i = 0; i < ITERATIONS; i++) {
+        barrier_wait(&barrier);
+        for (unsigned j = 0; j < size; j++) {
+            ptr[offset + j]++;
+        }
+        if (id == 0) sentinel++;
+        barrier_wait(&barrier);
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    assert(eclCompileSource(kernel) == eclSuccess);
+
+	assert(eclMalloc((void **)&ptr, totalSize * sizeof(unsigned)) == eclSuccess);
+
+	// Call the kernel
+    size_t globalSize = size_t(totalSize);
+    cl_mem tmp = cl_mem(eclPtr(ptr));
+
+    thread_t threads[THREADS];
+    unsigned ids[THREADS];
+
+    barrier_init(&barrier, THREADS + 1);
+
+    for (unsigned i = 0; i < THREADS; i++) {
+        ids[i] = i;
+        threads[i] = thread_create(func, &ids[i]);
+    }
+
+    for (unsigned i = 0; i < totalSize; i++) {
+        ptr[i] = 0;
+    }
+
+    barrier_wait(&barrier);
+
+    ecl_kernel kernel;
+
+    assert(eclGetKernel("inc", &kernel) == eclSuccess);
+
+    assert(eclSetKernelArg(kernel, 0, sizeof(cl_mem), &tmp) == eclSuccess);
+    assert(eclSetKernelArg(kernel, 1, sizeof(totalSize), &totalSize) == eclSuccess);
+
+    for (unsigned i = 0; i < ITERATIONS; i++) {
+        assert(eclCallNDRange(kernel, 1, NULL, &globalSize, NULL) == eclSuccess);
+        sentinel++;
+        barrier_wait(&barrier);
+        barrier_wait(&barrier);
+    }
+
+        for (unsigned i = 0; i < THREADS; i++) {
+         thread_wait(threads[i]);
+    }
+
+printf("Sentinel %u\n", sentinel);
+    for (unsigned i = 0; i < totalSize; i++) {
+        if (ptr[i] != 2 * ITERATIONS) {
+            printf("%u vs %u\n", ptr[i], 2 * ITERATIONS);
+            abort();
+        }
+    }
+
+
+
+    eclReleaseKernel(kernel);
+
+	eclFree(ptr);
+
+    return 0;
+}
