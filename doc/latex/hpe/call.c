d7fad71a5379034b620f23ea4368e6c56a945755
Isaac Gelado <igelado@illinois.edu>
2011-08-24 12:08:03 -0500

    Updated User Manual

--- doc/latex/hpe/call.c
+++ doc/latex/hpe/call.c
@@ -1,19 +1,18 @@
     ecl_kernel kernel;
     ecl_error error_code;
-    cl_mem mem;
     cl_uint global_size = vec_size;
 
     error_code = eclGetKernel("vecAdd", &kernel);
     if(error_code != eclSuccess) return error(error_code);
 
     error_code = eclSetKernelArgPtr(&kernel, 0, c);
-    if(mem == NULL || error_code != eclSuccess)
+    if(error_code != eclSuccess)
         return error(error_code);
     error_code = eclSetKernelArgPtr(&kernel, 1, a);
-    if(mem == NULL || error_code != eclSuccess)
+    if(error_code != eclSuccess)
         return error(error_code);
     error_code = eclSetKernelArgPtr(&kernel, 2, b);
-    if(mem == NULL || error_code != eclSuccess)
+    if(error_code != eclSuccess)
         return error(error_code);
 
     error_code = eclCallNDRange(&kernel, 1, NULL,
