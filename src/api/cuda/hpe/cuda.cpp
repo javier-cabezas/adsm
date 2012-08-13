a9a88ab00ed4024baccc10fe6cc53078f038a013
jcabezas <jcabezas@localhost>
2012-08-12 22:40:50 -0500

    Added the ability to hide devices in GMAC via environment variable

--- src/api/cuda/hpe/cuda.cpp
+++ src/api/cuda/hpe/cuda.cpp
@@ -1,8 +1,8 @@
 #include <cuda.h>
 
-#include <string>
+#include <algorithm>
 #include <list>
-
+#include <string>
 
 #include "api/cuda/hpe/Accelerator.h"
 #include "api/cuda/hpe/Mode.h"
@@ -13,6 +13,7 @@
 #include "hpe/init.h"
 
 #include "util/loader.h"
+#include "util/Parameter.h"
 
 static bool initialized = false;
 
@@ -30,8 +31,28 @@ void GMAC_API CUDA(gmac::core::hpe::Process &proc)
 
     TRACE(GLOBAL, "Found %d CUDA capable devices", devCount);
 
+    std::list<int> visibleDevices;
+    if (gmac::util::params::ParamVisibleDevices != NULL &&
+        strlen(gmac::util::params::ParamVisibleDevices) > 0) {
+        size_t len = strlen(gmac::util::params::ParamVisibleDevices);
+        char *visible = new char[len + 1];
+        ::memcpy(visible, gmac::util::params::ParamVisibleDevices, len + 1);
+
+        const char *strDev = strtok(visible, ",");
+        while (strDev != NULL) {
+            int dev = atoi(strDev);
+            visibleDevices.push_back(dev);
+            strDev = strtok(NULL, ",");
+        }
+
+        delete []visible;
+    }
+
     // Add accelerators to the system
     for(int i = 0; i < devCount; i++) {
+        if (visibleDevices.size() > 0 &&
+            std::find(visibleDevices.begin(), visibleDevices.end(), i) == visibleDevices.end()) continue;
+
         CUdevice cuDev;
         if(cuDeviceGet(&cuDev, i) != CUDA_SUCCESS)
             FATAL("Unable to access CUDA device");
@@ -39,7 +60,10 @@ void GMAC_API CUDA(gmac::core::hpe::Process &proc)
         int attr = 0;
         if(cuDeviceGetAttribute(&attr, CU_DEVICE_ATTRIBUTE_COMPUTE_MODE, cuDev) != CUDA_SUCCESS)
             FATAL("Unable to access CUDA device");
-        if(attr != CU_COMPUTEMODE_PROHIBITED) {
+
+        if(attr == CU_COMPUTEMODE_PROHIBITED) {
+	    TRACE(GLOBAL, "Device %d prohibited", i);
+	} else {
             gmac::cuda::hpe::Accelerator *accelerator = new gmac::cuda::hpe::Accelerator(i, cuDev);
             CFATAL(accelerator != NULL, "Error allocating resources for the accelerator");
             proc.addAccelerator(*accelerator);
@@ -56,7 +80,6 @@ void GMAC_API CUDA(gmac::core::hpe::Process &proc)
 
     // Initialize the private per-thread variables
     gmac::cuda::hpe::Accelerator::init();
-
 
     initialized = true;
 }
